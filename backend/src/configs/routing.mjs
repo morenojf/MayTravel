03076b1be100c586b72b79f9e039edfa97eaf733
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2026-03-26 16:27:12 -0400

    feat: add city-names endpoint.

--- backend/src/configs/routing.mjs
+++ backend/src/configs/routing.mjs
@@ -4,19 +4,18 @@ import { Router } from 'express'
 // middlewares
 import { validateInfo } from '../middlewares/UsersValid.mjs'
 
-
 // importación de controladores
-import { UsersController } from "../components/users/controller/UsersController.mjs"
+import { UsersController } from '../components/users/controller/UsersController.mjs'
 import { InterestController } from '../components/interests/controller/InterestsController.mjs'
 import { TripsController } from '../components/trips/controller/TripsController.mjs'
 import { PoisController } from '../components/poi_catalog/controller/PoisController.mjs'
 import { StopsController } from '../components/stops/controller/StopsController.mjs'
-
+import { CityNameController } from '../components/city-name/controller/CityNameController.mjs'
 
 // exportación de rutas
 export const routing = Router()
 
-// USERS 
+// USERS
 // managment (CRUD)
 routing.get('/users', UsersController.getAll)
 routing.get('/users/:id', UsersController.getById)
@@ -40,7 +39,6 @@ routing.get('/users/:id/trips', TripsController.getByUser)
 // trips-generate
 routing.post('/users/:id/trips', TripsController.create)
 
-
 // INTERESTS
 // managment (CRUD)
 routing.get('/interests', InterestController.getAll)
@@ -48,7 +46,7 @@ routing.post('/interests', InterestController.create)
 routing.delete('/interests/:id', InterestController.delete)
 routing.put('/interests/:id', InterestController.update)
 
-// TRIPS 
+// TRIPS
 // managment (CRUD)
 routing.get('/trips', TripsController.getAll)
 routing.get('/trips/:id', TripsController.getById)
@@ -66,4 +64,5 @@ routing.patch('/pois/:id', PoisController.update)
 routing.post('/stops', StopsController.create)
 routing.delete('/stops/:id', StopsController.delete)
 
-
+// TYPEAHEAD (name places)
+routing.get('/city-name/:q', CityNameController.get)
