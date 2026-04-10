3ffc80f65f422aad0c5310b0423597a53da33782
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2026-04-10 10:39:42 -0400

    feat: implement protected private routes and public routes (login)

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,18 +1,26 @@
 // importación de configuración del servidor
 import { app, port } from './configs/server.mjs'
+import validateToken from './middlewares/auth.mjs'
 
 // traduccion de req.body a json
 import express from 'express'
 
+// traduccion de cookies
+import cookieParser from 'cookie-parser'
+
 // middlewares
 import './middlewares/cors.mjs'
 
-// importación de rutas desde el archivo routing. mjs
+// importación de rutas (publics, protected)
+import { publicRouting } from './configs/public-routing.mjs'
 import { routing } from './configs/routing.mjs'
 
 // aplicamos middlewares y rutas antes de arrancar el servidor
 app.use(express.json())
-app.use('/', routing)
+app.use(cookieParser())
+
+app.use('/auth', publicRouting)
+app.use('/api', validateToken, routing)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
