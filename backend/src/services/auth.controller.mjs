e6fcaaeca3633cbdac3a2bf2e6c4978d4a2d59cc
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2026-05-07 21:32:24 -0400

    refactor: remove outdated comments from auth.controller.mjs

--- backend/src/services/auth.controller.mjs
+++ backend/src/services/auth.controller.mjs
@@ -1,15 +1,3 @@
-// once the user has loged in the signed string is generated
-// jwt.sign()
-
-// JWT STRUCT
-// xxxxx.yyyyy.zzzzz
-
-// HEADER
-// {
-//   "alg": "HS256",  algorithm used for encriptation
-//   "typ": "JWT" type of token
-// }
-
 import jwt from 'jsonwebtoken'
 
 export default function generateAccessToken(user) {
