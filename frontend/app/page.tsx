ea5dcb74735f4ea82ad46459c0933e917eac62d4
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2026-05-19 16:14:35 -0400

    fix: rollback and redeployment on Vercell

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,11 +2,9 @@
 import { redirect } from 'next/navigation'
 
 export default function RootPage() {
-  return <h1>¡El frontend en Vercel funciona!</h1>
+  // En el futuro, aquí verificarás si hay una sesión activa
+  // Si no hay sesión, lo mandamos al login
+  redirect('/login')
 
-  //   // En el futuro, aquí verificarás si hay una sesión activa
-  //   // Si no hay sesión, lo mandamos al login
-  //   redirect('/login')
-
-  //   return null // No renderiza nada porque redirige antes
+  return null // No renderiza nada porque redirige antes
 }
