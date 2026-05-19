73fe4443e0e0d7be0e5e7ba74e6a3f615460ea07
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2026-05-19 18:03:34 -0400

    fix: update CORS middleware to conditionally set origin based on environment

--- backend/src/middlewares/cors.mjs
+++ backend/src/middlewares/cors.mjs
@@ -2,10 +2,17 @@ import cors from 'cors'
 import { app } from '../configs/server.mjs'
 
 app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  })
+  process.env.NODE_ENV !== 'production'
+    ? cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+      })
+    : cors({
+        origin: 'https://may-travel-frontend-nextjs.vercel.app',
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+      })
 )
