2da19888cdb7779f3b8af90e49e05b024419d5f8
Francisco Javier Moreno Martínez <91105105+morenojf@users.noreply.github.com>
2026-05-19 18:04:53 -0400

    fix: update API URL constants to use environment variables for production env

--- frontend/app/lib/consts.ts
+++ frontend/app/lib/consts.ts
@@ -1,3 +1,3 @@
-export const PUBLIC_BASE_API_URL = 'http://localhost:4000/auth'
-export const BASE_API_URL = 'http://localhost:4000/api'
+export const PUBLIC_BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL || 'http://localhost:4000/auth'
+export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_PRIVATE_URL || 'http://localhost:4000/api'
 export const MAX_DISTANCE_KM = 10
